f40a491c0959eb5af7bafe7d06891fc1c0a7dbe6
Philine Görzig <philine@Tardis.fritz.box>
2019-05-21 18:05:10 +0200

    make it better

---
+++ events/message.js
@@ -0,0 +1,9 @@
+const kick = require('../commands/kick')
+module.exports = (client, message) => {
+  if (message.content.startsWith('!kick')) {
+    return kick(message)
+  }
+  if (message.content === 'ping') {
+    message.reply('Pong!')
+  }
+}
