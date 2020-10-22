f37eb908cc0b6742d078a3aac33a388ede61e4a7
Philine Görzig <philine@MacBook-Pro.fritz.box>
2020-10-22 14:08:54 +0200

    add server into separate file

---
+++ commands/addServer.js
@@ -0,0 +1,17 @@
+servers = {}
+setup = {
+  name: 'MIni-Match',
+  next: 'Next'
+}
+
+module.exports = (guild) => {
+
+  if (servers.hasOwnProperty(guild.id)) return
+
+  servers[guild.id] = {
+    voice_channels: [],
+    room_id: 0,
+    room_size: 2,
+    category_id: false
+  }
+}
