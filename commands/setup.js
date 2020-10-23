e24da99b5ee332cb22ae782c34f163f673fb1e46
Philine Görzig <philine@MacBook-Pro.fritz.box>
2020-10-23 22:37:50 +0200

    detailed setup instructions typo fix

--- commands/setup.js
+++ commands/setup.js
@@ -1,3 +1,3 @@
 module.exports = (client, message) => {
-  message.reply("create a category called `" + setup.name + "` and inside a voice channel called `" + setup.next + '`.\n\nThe category should set "Read Text Channels and see Voice Channels" to `X` for everyone. Then the Permissions of the `' + setup.next + ' Voice Channel should have "View Channel" set to ✔︎ for everyone.')
+  message.reply("create a category called `" + setup.name + "` and inside a Voice Channel called `" + setup.next + '`.\n\nThe category should set "Read Text Channels and see Voice Channels" to `X` for everyone. Then the Permissions of the `' + setup.next + '` Voice Channel should have "View Channel" set to ✔︎ for everyone.')
 }
